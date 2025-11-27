1417379ed888a05bcef9199a1aebbe7d2f520ed5
Fadydy <sammanfady66@gmail.com>
2025-11-27 23:47:17 +0200

    Add project files.

---
+++ PilotV/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows.Forms;
+using GTA;
+using GTA.Native;
+using GTA.Math;
+using LemonUI;
+using LemonUI.Menus;
+
+namespace PilotV //Your Mod Name Here
+{
+    public class Main : Script
+    {
+        ObjectPool pool = new ObjectPool();
+        NativeMenu mainMenu = new NativeMenu("Pilot Mod", "Main Menu");
+        public Main()
+        {
+            pool.Add(mainMenu);
+
+            GTA.UI.Notification.Show("Pilot mod by Fady"); //Notification on mod load
+            Tick += onTick;
+            KeyDown += onKeyDown;
+        }
+        private void onTick(object sender, EventArgs e)
+        {
+
+
+            pool.Process();
+        }
+        private void onKeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+    }
+}
